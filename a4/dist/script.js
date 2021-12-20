1511daf15871e6dd3f666503f290790c574f5e22
transparenceco <bruno@transparence.co>
2021-12-20 14:08:18 -0500

    Created A4

---
+++ a4/dist/script.js
@@ -0,0 +1,32 @@
+var container = document.getElementById("container");
+var circle = document.querySelector(".circle");
+
+TweenMax.set(".section", {
+  backgroundColor: function() {
+    return Math.random() * 0xffffff;
+  }
+});
+
+TweenMax.set(circle, { scale: 0, xPercent: -50, yPercent: -50 });
+
+container.addEventListener("pointerenter", function(e) {
+  TweenMax.to(circle, 0.3, { scale: 1, opacity: 1 });
+  positionCircle(e);
+});
+
+container.addEventListener("pointerleave", function(e) {
+  TweenMax.to(circle, 0.3, { scale: 0, opacity: 0 });
+  positionCircle(e);
+});
+
+container.addEventListener("pointermove", function(e) {
+  positionCircle(e);
+});
+
+function positionCircle(e) {
+  var rect = container.getBoundingClientRect();  
+  var relX = e.pageX - container.offsetLeft;
+  var relY = e.pageY - container.offsetTop;
+
+  TweenMax.to(circle, 0.3, { x: relX, y: relY });
+}
