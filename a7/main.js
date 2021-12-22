564afff2b2b4b2604a8398677c1d4e5507e0a27a
transparenceco <bruno@transparence.co>
2021-12-22 00:21:10 -0500

    Fixed A7 slides

--- a7/main.js
+++ a7/main.js
@@ -1,4 +1,4 @@
-const slides = document.querySelectorAll('slide');
+const slides = document.querySelectorAll('.slide');
 const next = document.querySelector('#next');
 const prev = document.querySelector('#prev');
 const auto = false; // Auto scroll
@@ -6,44 +6,56 @@ const intervalTime = 5000;
 let slideInterval;
 
 const nextSlide = () => {
-// Get current class
-    const current = document.querySelector('.current');
-// Remove current class
-    current.classList.remove('current');
-// check for next slide
-    if(current.nextElementSibling){
-        //add current to next sibling
-        current.nextElementSibling.classList.add('current');
-
-    }else{
-        //add current to start
-        slides[0].classList.add('current');
-        } 
-    setTimeout(() => current.classList.remove('current'));
-}
+  // Get current class
+  const current = document.querySelector('.current');
+  // Remove current class
+  current.classList.remove('current');
+  // Check for next slide
+  if (current.nextElementSibling) {
+    // Add current to next sibling
+    current.nextElementSibling.classList.add('current');
+  } else {
+    // Add current to start
+    slides[0].classList.add('current');
+  }
+  setTimeout(() => current.classList.remove('current'));
+};
 
 const prevSlide = () => {
-    // Get current class
-        const current = document.querySelector('.current');
-    // Remove current class
-        current.classList.remove('current');
-    // check for next slide
-        if(current.previousElementSibling){
-            //add current to prev sibling
-            current.previousElementSibling.classList.add('current');
-    
-        }else{
+  // Get current class
+  const current = document.querySelector('.current');
+  // Remove current class
+  current.classList.remove('current');
+  // Check for prev slide
+  if (current.previousElementSibling) {
+    // Add current to prev sibling
+    current.previousElementSibling.classList.add('current');
+  } else {
     // Add current to last
     slides[slides.length - 1].classList.add('current');
   }
   setTimeout(() => current.classList.remove('current'));
-    }
-    
+};
+
 // Button events
 next.addEventListener('click', e => {
-    nextSlide();
-})
-// Button events
+  nextSlide();
+  if (auto) {
+    clearInterval(slideInterval);
+    slideInterval = setInterval(nextSlide, intervalTime);
+  }
+});
+
 prev.addEventListener('click', e => {
-    prevSlide();
-})
+  prevSlide();
+  if (auto) {
+    clearInterval(slideInterval);
+    slideInterval = setInterval(nextSlide, intervalTime);
+  }
+});
+
+// Auto slide
+if (auto) {
+  // Run next slide at interval time
+  slideInterval = setInterval(nextSlide, intervalTime);
+}
